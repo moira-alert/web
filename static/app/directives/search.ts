5ec88d0aad250fbee14d17dd431ed5af3415979e
Anton.A.Zlygostev <bersegosx@gmail.com>
2016-09-18 15:38:54 +0500

    fix copy/paste

--- static/app/directives/search.ts
+++ static/app/directives/search.ts
@@ -6,19 +6,16 @@ export interface ISearchScope extends ng.IScope{
 }
 
 export function Search(): ng.IDirective {
-
-	const max_items:number = 99;
-
 	return {
 		restrict: 'E',
 		template: require('./templates/search.html'),
-
 		replace: true,
-		transclude: true,
+
 		scope: {
-			model:"=model",
-			placeholder:"@placeholder",
+			model: "=model",
+			placeholder: "@placeholder",
 		},
+
 		link: function (scope:ISearchScope, element:JQuery, attrs:ng.IAttributes) {
 			scope.clear = () => {
 				scope.model = "";
